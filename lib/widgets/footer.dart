1088d82e74b61967c61193b4601138c853869ce5
Henrique Nascimento <henriquenas.dev@gmail.com>
2023-04-16 14:19:05 -0300

    feat: :sparkles: create footer widget with actions and infos

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../config/spotify_icons.dart';
+
 class Footer extends StatelessWidget {
   const Footer({super.key});
 
@@ -7,6 +9,7 @@ class Footer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 120,
+      padding: const EdgeInsets.all(16),
       decoration: const BoxDecoration(
         color: Color(0xFF181818),
         border: Border(
@@ -16,6 +19,153 @@ class Footer extends StatelessWidget {
           ),
         ),
       ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _PlayerInfo(),
+          _PlayerActions(),
+          _SystemControls(),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlayerInfo extends StatelessWidget {
+  const _PlayerInfo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+          child: const Placeholder(),
+        ),
+        const SizedBox(width: 16),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Top 50 Global',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              'Spotify',
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.5),
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _PlayerActions extends StatelessWidget {
+  const _PlayerActions();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            IconButton(onPressed: () {}, icon: Icon(Spotify.shuffle)),
+            IconButton(onPressed: () {}, icon: Icon(Spotify.to_start)),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Icon(Spotify.play_1, color: Color(0xFF181818)),
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(12),
+                alignment: Alignment.center,
+                backgroundColor: Colors.white,
+              ),
+            ),
+            IconButton(onPressed: () {}, icon: Icon(Spotify.to_end)),
+            IconButton(onPressed: () {}, icon: Icon(Spotify.loop)),
+          ],
+        ),
+        const SizedBox(height: 16),
+        Row(
+          children: [
+            Text(
+              '2:10',
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.5),
+                fontSize: 12,
+              ),
+            ),
+            const SizedBox(width: 8),
+            SizedBox(
+              width: 300,
+              child: LinearProgressIndicator(
+                value: 0.5,
+                valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+                backgroundColor: Colors.white.withOpacity(0.2),
+              ),
+            ),
+            const SizedBox(width: 8),
+            Text(
+              '4:20',
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.5),
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _SystemControls extends StatefulWidget {
+  const _SystemControls();
+
+  @override
+  State<_SystemControls> createState() => _SystemControlsState();
+}
+
+class _SystemControlsState extends State<_SystemControls> {
+  double _sound = 0.6;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(onPressed: () {}, icon: Icon(Spotify.list)),
+        IconButton(onPressed: () {}, icon: Icon(Spotify.monitor)),
+        IconButton(
+          onPressed: () => setState(() => _sound = _sound == 0 ? 0.6 : 0),
+          icon: Icon(
+            _sound == 0 //
+                ? Spotify.mute
+                : Spotify.sound,
+          ),
+        ),
+        SizedBox(
+          width: 120,
+          child: Slider(
+            value: _sound,
+            onChanged: (value) => setState(() => _sound = value),
+            activeColor: Colors.white,
+            inactiveColor: Colors.white.withOpacity(0.2),
+          ),
+        ),
+        IconButton(onPressed: () {}, icon: Icon(Spotify.resize_full)),
+      ],
     );
   }
 }
